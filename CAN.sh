10b5442b2d481e904e00f0af6c4e0d976741e0d4
Samuel Duclos <wiseakasha@hotmail.com>
2019-11-30 22:38:16 -0500

    Update CAN.sh

--- CAN.sh
+++ CAN.sh
@@ -4,26 +4,25 @@
 #                This prevents file descriptors from getting
 #                closed prematurely with an exiting subshell.
 # Documentation: https://github.com/linux-can/can-utils/blob/master/bcmserver.c
-# Example usage: source /home/debian/labs/CAN/CAN.sh
+# Example usage: source /home/debian/CAN.sh
 
 # Modifiable variables.
 INTERFACE_TYPE=vcan
-DELAY_SEC=0
 DELAY_US=500000
 
 # Non-modifiable variables.
-INTERFACE=$INTERFACE_TYPE"0"
+INTERFACE=$INTERFACE_TYPE'0'
 SOCKET=/dev/tcp/127.0.0.1/28600
 GREEN_LED=/sys/class/leds/green
 RED_LED=/sys/class/leds/red
 
 # Set green LED to persistent mode.
-echo "none" > $GREEN_LED/trigger
-echo "none" > $RED_LED/trigger
+echo 'none' > $GREEN_LED/trigger
+echo 'none' > $RED_LED/trigger
 
 # Turn green LED on and red LED off.
-echo "255" > $GREEN_LED/brightness
-echo "0" > $RED_LED/brightness
+echo '255' > $GREEN_LED/brightness
+echo '0' > $RED_LED/brightness
 
 if [ ! -e /dev/tcp ]; then
     # Create character device for TCP/IP socket manipulation if it doesn't exist already.
@@ -34,7 +33,7 @@ if [ ! -e /dev/tcp ]; then
 fi
 
 # Setup interface if it doesn't exist.
-if [ ! -e "/sys/class/net/"$INTERFACE ]; then
+if [ ! -e /sys/class/net/$INTERFACE ]; then
     ip link add name $INTERFACE type $INTERFACE_TYPE
 fi
 
@@ -44,9 +43,7 @@ ifconfig $INTERFACE up
 # Starting CAN broadcast manager.
 bcmserver &
 PID_SERVER=$!
-
-# Waiting for server to boot.
-sleep 1
+sleep 1 # Waiting for server to boot.
 
 # Linking socket to file descriptor to be used.
 exec 3<>$SOCKET
@@ -55,10 +52,35 @@ exec 3<>$SOCKET
 cat <&3 &
 PID_INPUT=$!
 
+function destroy {
+    # Be nice and kill all nodes.
+    echo "<${INTERFACE} X 0 0 010 0 00>" >&3 # Delete receiver.
+    echo "<${INTERFACE} D 0 0 010 0 00>" >&3 # Delete cyclic sender.
+    echo "<${INTERFACE} S 0 0 010 1 00>" >&3 # Send power off command.
+    sleep 1
+
+    # Kill server.
+    kill $PID_SERVER
+
+    # Destroy network interface.
+    ifconfig $INTERFACE down
+    ip link delete dev $INTERFACE type $INTERFACE_TYPE
+
+    # Turn green LED off and red LED on.
+    echo '0' > $GREEN_LED/brightness
+    echo '255' > $RED_LED/brightness
+}
+
+# Let kernel trap shell exit interruptions.
+trap destroy EXIT
+trap destroy SIGTERM
+trap destroy SIGINT
+trap destroy SIGHUP
+
 # Send commands to server through file descriptor to socket:
 
 # < INTERFACE ADD_CYCLE DELAY_SEC DELAY_US HEX_ADDRESS N_BYTES SPACE_SEPARATED_HEX_BYTES >
-echo "<${INTERFACE} A ${DELAY_SEC} ${DELAY_US} 010 1 01>" >&3
+echo "<${INTERFACE} A 0 ${DELAY_US} 010 1 01>" >&3
 
 # < INTERFACE RECEIVE_FROM DELAY_SEC DELAY_US HEX_ADDRESS N_BYTES FILTER >
 echo "<${INTERFACE} R 0 0 010 1 FF>" >&3
@@ -73,35 +95,12 @@ while true :; do
     elif [ "$SYNC" == 'ON' ]; then
         continue
     elif [ "$SYNC" == 'OFF' ]; then
-        echo "Power down command received!"
+        echo 'Power down command received!'
         break
     else
-        echo "Ill command received from computer."
+        echo 'Ill command received from computer.'
         break
     fi
 done
 
-# Kill file descriptor input.
-kill $PID_INPUT
-sleep 1
-
-# Destroy file descriptor.
-exec 3>&-
-
-# Be nice and kill all nodes.
-echo "<${INTERFACE} X 0 0 010 0 00>" >&3 # Delete receiver.
-echo "<${INTERFACE} D 0 0 010 0 00>" >&3 # Delete cyclic sender.
-echo "<${INTERFACE} S 0 0 010 1 00>" >&3 # Send power off command.
-sleep 1
-
-# Kill server.
-kill $PID_SERVER
-sleep 1
-
-# Destroy network interface.
-ifconfig $INTERFACE down
-ip link delete dev $INTERFACE type $INTERFACE_TYPE
-
-# Turn green LED off and red LED on.
-echo "0" > $GREEN_LED/brightness
-echo "255" > $RED_LED/brightness
+destroy
